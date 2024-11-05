ad5f38731f792636e05e39a7a850088f53d6cf16
Candela.Gomez.18 <cn.gomez18@gmail.com>
2024-11-05 00:00:20 -0300

    genero y actor rutas con swagger

--- src/routes/actorRoutes.js
+++ src/routes/actorRoutes.js
@@ -2,6 +2,33 @@ const express = require('express');
 const router = express.Router();
 const { Actor } = require('../models/actor');
 
+/**
+ * @swagger
+ * /actor:
+ *   get:
+ *     summary: Obtener todos los actores
+ *     description: Endpoint para obtener una lista de todos los actores en la base de datos.
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa. Devuelve una lista de actores.
+ *         content:
+ *           application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Actor'
+ *       404:
+ *         description: No se encontraron actores para listar.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: No se encontraron actores para listar.
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error en el servidor
+ */
+
 router.get('/', async (req, res) => {
   try {
     const actors = await Actor.findAll();
@@ -17,6 +44,40 @@ router.get('/', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /actor/{id}:
+ *   get:
+ *     summary: Obtener un actor por ID
+ *     description: Endpoint para obtener los detalles de un actor específico por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del actor
+ *     responses:
+ *       200:
+ *         description: Actor encontrado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Actor'
+ *       404:
+ *         description: No se encontró el actor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: No se encontró el actor
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error en el servidor
+ */
+
 router.get('/:id', async (req, res) => {
   try {
     const actor = await Actor.findByPk(req.params.id);
@@ -31,6 +92,35 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /actor:
+ *   post:
+ *     summary: Crear un nuevo actor
+ *     description: Endpoint para crear un nuevo actor en la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               first_name:
+ *                 type: string
+ *               last_name:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Actor creado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Actor'
+ *       400:
+ *         description: Nombre y apellido son obligatorios.
+ *       500:
+ *         description: Error al crear el actor.
+ */
 
 router.post('/', async (req, res) => {
   const { first_name, last_name } = req.body;
@@ -46,6 +136,45 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Error al crear un actor' });
   }
 });
+
+/**
+ * @swagger
+ * /actor/{id}:
+ *   put:
+ *     summary: Actualizar un actor
+ *     description: Endpoint para actualizar los datos de un actor existente.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del actor a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               first_name:
+ *                 type: string
+ *               last_name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Actor actualizado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Actor'
+ *       400:
+ *         description: Nombre y apellido son obligatorios.
+ *       404:
+ *         description: No se encontró el actor para actualizar.
+ *       500:
+ *         description: Error al actualizar el actor.
+ */
 
 router.put('/:id', async (req, res) => {
   const { first_name, last_name } = req.body;
@@ -68,6 +197,28 @@ router.put('/:id', async (req, res) => {
     res.status(500).json({ error: 'Error al actualizar al actor' });
   }
 });
+
+/**
+ * @swagger
+ * /actor/{id}:
+ *   delete:
+ *     summary: Eliminar un actor
+ *     description: Endpoint para eliminar un actor de la base de datos por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del actor a eliminar
+ *     responses:
+ *       204:
+ *         description: Actor eliminado exitosamente.
+ *       404:
+ *         description: No se encontró el actor para eliminar.
+ *       500:
+ *         description: Error al eliminar el actor.
+ */
 
 router.delete('/:id', async (req, res) => {
   try {
--- src/routes/generoRoutes.js
+++ src/routes/generoRoutes.js
@@ -2,6 +2,36 @@ const express = require('express');
 const { Genero } = require('../models/genero');
 const router = express.Router();
 
+/**
+ * @swagger
+ * /genero:
+ *   get:
+ *     summary: Obtener todos los géneros
+ *     description: Endpoint para obtener una lista de todos los géneros en la base de datos.
+ *     responses:
+ *       200:
+ *         description: Lista de géneros obtenida exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - id: 1
+ *                 genre_name: Acción
+ *               - id: 2
+ *                 genre_name: Comedia
+ *       404:
+ *         description: No se encontraron géneros.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: No se encontraron géneros
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error al obtener géneros
+ */
+
 router.get('/', async (req, res) => {
   try {
     const genres = await Genero.findAll();
@@ -15,6 +45,41 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener géneros' });
   }
 });
+
+/**
+ * @swagger
+ * /genero/{id}:
+ *   get:
+ *     summary: Obtener un género por ID
+ *     description: Endpoint para obtener un género específico usando su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del género a obtener
+ *     responses:
+ *       200:
+ *         description: Género encontrado.
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               genre_name: Acción
+ *       404:
+ *         description: Género no encontrado.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Género no encontrado
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error al obtener género
+ */
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
@@ -31,6 +96,44 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /genero:
+ *   post:
+ *     summary: Crear un nuevo género
+ *     description: Endpoint para crear un género en la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               genre_name:
+ *                 type: string
+ *                 description: Nombre del género
+ *                 example: Ciencia Ficción
+ *     responses:
+ *       201:
+ *         description: Género creado exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 3
+ *               genre_name: Ciencia Ficción
+ *       400:
+ *         description: Datos faltantes o inválidos.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: El nombre del genero es obligatorio
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error al crear el genero
+ */
 
 router.post('/', async (req, res) => {
   const { genre_name } = req.body;
@@ -46,6 +149,58 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Error al crear el genero' });
   }
 });
+
+/**
+ * @swagger
+ * /genero/{id}:
+ *   put:
+ *     summary: Actualizar un género existente
+ *     description: Endpoint para actualizar un género específico por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del género a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               genre_name:
+ *                 type: string
+ *                 description: Nombre actualizado del género
+ *                 example: Drama
+ *     responses:
+ *       200:
+ *         description: Género actualizado exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               genre_name: Drama
+ *       400:
+ *         description: Datos faltantes o inválidos.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: El nombre del genero es obligatorio
+ *       404:
+ *         description: Género no encontrado.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: No se pudo encontrar el genero
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error al actualizar el genero
+ */
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
@@ -68,6 +223,36 @@ router.put('/:id', async (req, res) => {
     res.status(500).json({ error: 'Error al actualizar el genero' });
   }
 });
+
+/**
+ * @swagger
+ * /genero/{id}:
+ *   delete:
+ *     summary: Eliminar un género
+ *     description: Endpoint para eliminar un género específico usando su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del género a eliminar
+ *     responses:
+ *       204:
+ *         description: Género eliminado exitosamente (sin contenido).
+ *       404:
+ *         description: Género no encontrado.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: No se pudo encontrar el genero
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Error al eliminar el genero
+ */
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
